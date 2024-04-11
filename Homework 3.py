36ad28633a00a06d213c1b3e68d6f7c640a73aba
Ryan Hensley <ryanhensleyy@outlook.com>
2024-04-11 15:21:31 -0500

    Added HW3

--- Homework 3.py
+++ Homework 3.py
@@ -1,5 +1,6 @@
 #Ryan Hensley 4/1/2023
 
+<<<<<<< HEAD
 #imports
 import pandas as pd
 import requests
@@ -30,3 +31,24 @@ print(results_df)
 
 # Save the data to a JSON file
 results_df.to_json("hardware_stores_LA.json", orient="records")
+=======
+'''
+Create a REST query that uses SoQL parameters to accomplish the following:
+
+    return three specific columns
+    limit the rows returned by applying criteria to two different columns (using the $where parameter). 
+
+Print out some information from the data obtained
+Save the data that results from your query in a JSON file.
+'''
+
+#imports
+    import pandas as pd
+    import requests as rest
+
+#set endpoint
+    endpoint = "https://data.lacity.org/resource/r4uk-afju.json?$query=SELECT%0A%20%20%60location_account%60%2C%0A%20%20%60business_name%60%2C%0A%20%20%60dba_name%60%2C%0A%20%20%60street_address%60%2C%0A%20%20%60city%60%2C%0A%20%20%60zip_code%60%2C%0A%20%20%60location_description%60%2C%0A%20%20%60mailing_address%60%2C%0A%20%20%60mailing_city%60%2C%0A%20%20%60mailing_zip_code%60%2C%0A%20%20%60naics%60%2C%0A%20%20%60primary_naics_description%60%2C%0A%20%20%60council_district%60%2C%0A%20%20%60location_start_date%60%2C%0A%20%20%60location_end_date%60%2C%0A%20%20%60location%60%2C%0A%20%20%60%3A%40computed_region_2dna_qi2s%60%2C%0A%20%20%60%3A%40computed_region_ur2y_g4cx%60%2C%0A%20%20%60%3A%40computed_region_tatf_ua23%60%2C%0A%20%20%60%3A%40computed_region_kqwf_mjcx%60%2C%0A%20%20%60%3A%40computed_region_k96s_3jcv%60%2C%0A%20%20%60%3A%40computed_region_qz3q_ghft%60"
+
+#SoQL params
+    
+>>>>>>> 39e9008c3284cb3d7de1891150dbf0a1233fb4da
